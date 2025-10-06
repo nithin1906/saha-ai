f33e9c7fbb4ad663b66253bff4dc37acbc171c15
nithin196 <nithin.n.official@gmail.com>
2025-10-06 14:38:04 +0530

    CRITICAL FIX: Add DeviceDetectionMiddleware to production middleware list

--- core/settings_production.py
+++ core/settings_production.py
@@ -127,8 +127,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'advisor.device_routing.DeviceDetectionMiddleware',  # Add device detection middleware
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
